00000fb638a63b0831677782f50fa5573f5bff9f
syaofox <syaofox@gmail.com>
2026-05-11 23:11:08 +0800

    slock 从主分支克隆

--- setup/install-slock.sh
+++ setup/install-slock.sh
@@ -4,5 +4,5 @@ source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
 log_step "Compile and install slock..."
 
-compile_and_install "slock" "https://github.com/syaofox/slock.git" "/tmp/slock" "arch"
+compile_and_install "slock" "https://github.com/syaofox/slock.git" "/tmp/slock"
 exit $?
