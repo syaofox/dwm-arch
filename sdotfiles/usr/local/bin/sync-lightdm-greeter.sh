cce325c200d0c73b8ccbd11c94ad870d96fe511d
syaofox <syaofox@gmail.com>
2026-05-14 20:02:22 +0800

    lightdm切换主题2

--- sdotfiles/usr/local/bin/sync-lightdm-greeter.sh
+++ sdotfiles/usr/local/bin/sync-lightdm-greeter.sh
@@ -10,9 +10,9 @@ fi
 THEME=$(cat "$USER_HOME/.config/theme" 2>/dev/null || echo "tokyonight")
 THEME_FILE="$USER_HOME/.Xresources.d/$THEME"
 
-GTK_THEME=$(grep -m1 '^#define\s\+GTK_THEME\s\+' "$THEME_FILE" 2>/dev/null | awk '{print $3}') || GTK_THEME="Mint-Y-Purple"
-GTK_ICON=$(grep -m1 '^#define\s\+GTK_ICON\s\+' "$THEME_FILE" 2>/dev/null | awk '{print $3}') || GTK_ICON="Mint-Y-Purple"
-GTK_FONT=$(grep -m1 '^#define\s\+GTK_FONT\s\+' "$THEME_FILE" 2>/dev/null | awk '{print $3}') || GTK_FONT="Noto Sans 11"
+GTK_THEME=$(grep -m1 '^#define\s\+LIGHTDM_GTK_THEME\s\+' "$THEME_FILE" 2>/dev/null | sed 's/^#define\s\+LIGHTDM_GTK_THEME\s\+//') || GTK_THEME=$(grep -m1 '^#define\s\+GTK_THEME\s\+' "$THEME_FILE" 2>/dev/null | sed 's/^#define\s\+GTK_THEME\s\+//') || GTK_THEME="Mint-Y-Purple"
+GTK_ICON=$(grep -m1 '^#define\s\+LIGHTDM_GTK_ICON\s\+' "$THEME_FILE" 2>/dev/null | sed 's/^#define\s\+LIGHTDM_GTK_ICON\s\+//') || GTK_ICON=$(grep -m1 '^#define\s\+GTK_ICON\s\+' "$THEME_FILE" 2>/dev/null | sed 's/^#define\s\+GTK_ICON\s\+//') || GTK_ICON="Mint-Y-Purple"
+GTK_FONT=$(grep -m1 '^#define\s\+GTK_FONT\s\+' "$THEME_FILE" 2>/dev/null | sed 's/^#define\s\+GTK_FONT\s\+//') || GTK_FONT="Noto Sans 11"
 WALLPAPER=$(cat "$USER_HOME/.config/wallpaper.conf" 2>/dev/null || echo "$USER_HOME/.config/walls/nord-2.png")
 
 mkdir -p /etc/lightdm
