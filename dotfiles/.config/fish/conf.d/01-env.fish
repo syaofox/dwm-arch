876fd3440eff3542a6bf77bcd07fa17587356758
syaofox <syaofox@gmail.com>
2026-06-18 14:18:23 +0800

    添加安卓开发变量

--- dotfiles/.config/fish/conf.d/01-env.fish
+++ dotfiles/.config/fish/conf.d/01-env.fish
@@ -6,6 +6,13 @@ set -gx UV_CACHE_DIR "/mnt/github/.caches/uv"
 set -gx BAT_THEME "Nord"
 set -gx NVM_DIR "$HOME/.config/nvm"
 
+# 设置 Android SDK 根目录
+set -gx ANDROID_HOME $HOME/Android/Sdk
+
+# 批量将 Android 工具链加入 PATH
+# Fish 允许直接向 $PATH 追加一个列表
+set -g fish_user_paths $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $ANDROID_HOME/cmdline-tools/latest/bin $fish_user_paths
+
 if test -f ~/.fzf/shell/key-bindings.fish
     source ~/.fzf/shell/key-bindings.fish
 end
